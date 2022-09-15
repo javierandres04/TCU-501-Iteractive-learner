8d07e3e8321b5af68d0b0244c26345e6c89ebf41
Sebastian Alfaro <alfaroleonsebas97@gmail.com>
2022-09-15 04:28:12 -0600

    diseño logos, paleta de colores.

--- interactive-learner/src/components/Footer/Footer.js
+++ interactive-learner/src/components/Footer/Footer.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './Footer.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import logoUCR from './ucr.png';
+import logoTCU from './TCU.png';
 
 export const Footer = () => {
     return (
         <>
             <Navbar id='footer' bg="clear" variant="clear">
-                <Container id="copyright">
-                    <Navbar.Brand  href="#home">© Sebastian Alfaro, Javier Molina </Navbar.Brand>
+                <Container id="footerContainer">
+                    <img className='logos' src={logoUCR} alt="logo" />
+                    <img className='logos' src={logoTCU} alt="logo" />
+                    <Navbar.Brand  href="#home">© Sebastián Alfaro, Javier Molina </Navbar.Brand>
                 </Container>
             </Navbar>
         </>
--- interactive-learner/src/components/Header/Header.js
+++ interactive-learner/src/components/Header/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Header.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../logo/logo_transparent.png';
 
 export const Header = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <img src="aaa"></img>
+            <Navbar id='header'>
+                <Container id='myContainer'>
+                    <img id='logoApp' src={logo} alt="logo" />
                     <Navbar.Brand href="#home">Interactive English Learner</Navbar.Brand>
                 </Container>
             </Navbar>
