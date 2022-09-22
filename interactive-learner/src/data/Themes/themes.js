00405890e5e70b61515925f951e499740e9333c0
Javier Molina Herrera <javmoli045@gmail.com>
2022-09-21 21:30:51 -0600

    Agregada estructura inicial del json de temas

---
+++ interactive-learner/src/data/Themes/themes.js
@@ -0,0 +1,78 @@
+export const themes = [
+  {
+    id: 1,
+    grade: "4th",
+    units: [
+      {
+        unit: 1,
+        themes: [
+          {
+            name: "Weather and Seasons",
+            route: ""
+          },
+          {
+            name: "Sunshine and Rainbows-my Feelings",
+            route: ""
+          },
+          {
+            name: "What to Wear?",
+            route: ""
+          },
+          {
+            name: `“Walking in the Rain”-Indoor and Outdoor Activities`,
+            route: ""
+          }
+        ]
+      },
+      {
+        unit: 2,
+        themes: [
+          {
+            name: "Places and People at the Park",
+            route: ""
+          },
+          {
+            name: "Wildlife at the Park",
+            route: ""
+          }
+        ]
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    grade: "5th",
+    units: [
+      {
+        unit: 1,
+        themes: [
+          {
+            name: "Our Environment: Making the Connection",
+            route: ""
+          }, {
+            name: "Protecting my Planet",
+            route: ""
+          },
+          {
+            name: "Solving Environmental Problems",
+            route: ""
+          }
+        ]
+      },
+      {
+        unit: 2,
+        themes: [
+          {
+            name: `What's Going on in Town?`,
+            route: ""
+          }, {
+            name: "Countries and Nationalities",
+            route: ""
+          }
+        ]
+      }
+    ]
+  }
+]
+
