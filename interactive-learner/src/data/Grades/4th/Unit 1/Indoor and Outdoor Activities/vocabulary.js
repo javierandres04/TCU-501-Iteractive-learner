4b00aa8cff6b92e3530a9523afaa0d364f6d385c
Javier Molina Herrera <javmoli045@gmail.com>
2022-10-18 20:45:05 -0600

    Modificados los archivos js de vocabulario

--- interactive-learner/src/data/Grades/4th/Unit 1/Indoor and Outdoor Activities/vocabulary.js
+++ interactive-learner/src/data/Grades/4th/Unit 1/Indoor and Outdoor Activities/vocabulary.js
@@ -1,155 +1,249 @@
 export const vocabulary = [
 
-    {
-      word: 'Fishing',
-      imageSrc: '/images/Fishing.png',
-      soundSrc: '/sounds/Fishing.mp3'
-    },
-    
-    
-    {
-      word: 'Gardening',
-      imageSrc: '/images/Gardening.png',
-      soundSrc: '/sounds/Gardening.mp3'
-    },
-    {
-      word: 'Climbing',
-      imageSrc: '/images/Climbing.png',
-      soundSrc: '/sounds/Climbing.mp3'
-    },
-    {
-      word: 'Swimming',
-      imageSrc: '/images/Swimming.png',
-      soundSrc: '/sounds/Swimming.mp3'
-    },
-    {
-      word: 'Bungee Jumping',
-      imageSrc: '/images/Bungee Jumping.png',
-      soundSrc: '/sounds/Bungee Jumping.mp3'
-    },
-    {
-      word: 'Cycling',
-      imageSrc: '/images/Cycling.png',
-      soundSrc: '/sounds/Cycling.mp3'
-    },
-    {
-      word: 'Running',
-      imageSrc: '/images/Running.png',
-      soundSrc: '/sounds/Running.mp3'
-    },
-    {
-      word: 'Walking',
-      imageSrc: '/images/Walking.png',
-      soundSrc: '/sounds/Walking.mp3'
-    },
-    {
-      word: 'Surfing',
-      imageSrc: '/images/Surfing.png',
-      soundSrc: '/sounds/Surfing.mp3'
-    },
-    {
-      word: 'Skateboarding',
-      imageSrc: '/images/Skateboarding.png',
-      soundSrc: '/sounds/Skateboarding.mp3'
-    },
-    {
-      word: 'Water Skiing',
-      imageSrc: '/images/Water Skiing.png',
-      soundSrc: '/sounds/Water Skiing.mp3'
-    },
-    {
-      word: 'Snowboarding',
-      imageSrc: '/images/Snowboarding.png',
-      soundSrc: '/sounds/Snowboarding.mp3'
-    },
-    {
-      word: 'Boxing',
-      imageSrc: '/images/Boxing.png',
-      soundSrc: '/sounds/Boxing.mp3'
-    },
-    {
-      word: 'Skiing',
-      imageSrc: '/images/Skiing.png',
-      soundSrc: '/sounds/Skiing.mp3'
-    },
-    {
-      word: 'Playing Golf',
-      imageSrc: '/images/Playing Golf.png',
-      soundSrc: '/sounds/Playing Golf.mp3'
-    },
-    {
-      word: 'Playing Volleyball',
-      imageSrc: '/images/Playing Volleyball.png',
-      soundSrc: '/sounds/Playing Volleyball.mp3'
-    },
-    {
-      word: 'Playing Tennis',
-      imageSrc: '/images/Playing Tennis.png',
-      soundSrc: '/sounds/Playing Tennis.mp3'
-    },
-    {
-      word: 'Playing Hockey',
-      imageSrc: '/images/Playing Hockey.png',
-      soundSrc: '/sounds/Playing Hockey.mp3'
-    },
-    {
-      word: 'Playing Baseball',
-      imageSrc: '/images/Playing Baseball.png',
-      soundSrc: '/sounds/Playing Baseball.mp3'
-    },
-    {
-      word: 'Horseback Riding',
-      imageSrc: '/images/Horseback Riding.png',
-      soundSrc: '/sounds/Horseback Riding.mp3'
-    },
-    {
-      word: 'Picnicking',
-      imageSrc: '/images/Picnicking.png',
-      soundSrc: '/sounds/Picnicking.mp3'
-    },
-    {
-      word: 'Camping',
-      imageSrc: '/images/Camping.png',
-      soundSrc: '/sounds/Camping.mp3'
-    },
-    {
-      word: 'Playing Cards',
-      imageSrc: '/images/Playing Cards.png',
-      soundSrc: '/sounds/Playing Cards.mp3'
-    },
-    {
-      word: 'Playing Board Games',
-      imageSrc: '/images/Playing Board Games.png',
-      soundSrc: '/sounds/Playing Board Games.mp3'
-    },
-    {
-      word: 'Playing Video Games',
-      imageSrc: '/images/Playing Video Games.png',
-      soundSrc: '/sounds/Playing Video Games.mp3'
-    },
-    {
-      word: 'Listening To Music',
-      imageSrc: '/images/Listening To Music.png',
-      soundSrc: '/sounds/Listening To Music.mp3'
-    },
-    {
-      word: 'Watching TV',
-      imageSrc: '/images/Watching Movies.png',
-      soundSrc: '/sounds/Watching Movies.mp3'
-    },
-    {
-      word: 'Reading',
-      imageSrc: '/images/Reading.png',
-      soundSrc: '/sounds/Reading.mp3'
-    },
-    {
-      word: 'Studying',
-      imageSrc: '/images/Studying.png',
-      soundSrc: '/sounds/Studying.mp3'
-    },
-    {
-      word: 'Doing Yoga',
-      imageSrc: '/images/Doing Yoga.png',
-      soundSrc: '/sounds/Doing Yoga.mp3'
-    }
+  {
+    word: 'Fishing',
+    imageSrc: '/images/Fishing.png',
+    soundSrc: '/sounds/Fishing.mp3'
+  },
+  {
+    word: 'Gardening',
+    imageSrc: '/images/Gardening.png',
+    soundSrc: '/sounds/Gardening.mp3'
+  },
+  {
+    word: 'Climbing',
+    imageSrc: '/images/Climbing.png',
+    soundSrc: '/sounds/Climbing.mp3'
+  },
+  {
+    word: 'Kayaking',
+    imageSrc: '/images/Kayaking.png',
+    soundSrc: '/sounds/Kayaking.mp3'
+  },
+  {
+    word: 'Hiking',
+    imageSrc: '/images/Hiking.png',
+    soundSrc: '/sounds/Hiking.mp3'
+  },
+  {
+    word: 'Swimming',
+    imageSrc: '/images/Swimming.png',
+    soundSrc: '/sounds/Swimming.mp3'
+  },
+  {
+    word: 'Bungee Jumping',
+    imageSrc: '/images/Bungee Jumping.png',
+    soundSrc: '/sounds/Bungee Jumping.mp3'
+  },
+  {
+    word: 'Cycling',
+    imageSrc: '/images/Cycling.png',
+    soundSrc: '/sounds/Cycling.mp3'
+  },
+  {
+    word: 'Running',
+    imageSrc: '/images/Running.png',
+    soundSrc: '/sounds/Running.mp3'
+  },
+  {
+    word: 'Walking',
+    imageSrc: '/images/Walking.png',
+    soundSrc: '/sounds/Walking.mp3'
+  },
+  {
+    word: 'Surfing',
+    imageSrc: '/images/Surfing.png',
+    soundSrc: '/sounds/Surfing.mp3'
+  },
+  {
+    word: 'Skating',
+    imageSrc: '/images/Skating.png',
+    soundSrc: '/sounds/Skating.mp3'
+  },
+  {
+    word: 'Skateboarding',
+    imageSrc: '/images/Skateboarding.png',
+    soundSrc: '/sounds/Skateboarding.mp3'
+  },
+  {
+    word: 'Water Skiing',
+    imageSrc: '/images/Water_Skiing.png',
+    soundSrc: '/sounds/Water_Skiing.mp3'
+  },
+  {
+    word: 'Snowboarding',
+    imageSrc: '/images/Snowboarding.png',
+    soundSrc: '/sounds/Snowboarding.mp3'
+  },
+  {
+    word: 'Boxing',
+    imageSrc: '/images/Boxing.png',
+    soundSrc: '/sounds/Boxing.mp3'
+  },
+  {
+    word: 'Skiing',
+    imageSrc: '/images/Skiing.png',
+    soundSrc: '/sounds/Skiing.mp3'
+  },
+  {
+    word: 'Golfing',
+    imageSrc: '/images/Golfing.png',
+    soundSrc: '/sounds/Golfing.mp3'
+  },
+  {
+    word: 'Playing Volleyball',
+    imageSrc: '/images/Playing_Volleyball.png',
+    soundSrc: '/sounds/Playing_Volleyball.mp3'
+  },
+  {
+    word: 'Playing Tennis',
+    imageSrc: '/images/Playing_Tennis.png',
+    soundSrc: '/sounds/Playing_Tennis.mp3'
+  },
+  {
+    word: 'Playing Hockey',
+    imageSrc: '/images/Playing_Hockey.png',
+    soundSrc: '/sounds/Playing_Hockey.mp3'
+  },
+  {
+    word: 'Playing Baseball',
+    imageSrc: '/images/Playing_Baseball.png',
+    soundSrc: '/sounds/Playing_Baseball.mp3'
+  },
+  {
+    word: 'Playing Basketball',
+    imageSrc: '/images/Playing_Basketball.png',
+    soundSrc: '/sounds/Playing_Basketball.mp3'
+  },
+  {
+    word: 'Horseback Riding',
+    imageSrc: '/images/Horseback_Riding.png',
+    soundSrc: '/sounds/Horseback_Riding.mp3'
+  },
+  {
+    word: 'Doing Picnic',
+    imageSrc: '/images/Doing_Picnic.png',
+    soundSrc: '/sounds/Doing_Picnic.mp3'
+  },
+  {
+    word: 'Camping',
+    imageSrc: '/images/Camping.png',
+    soundSrc: '/sounds/Camping.mp3'
+  },
+  {
+    word: 'Playing Board Games',
+    imageSrc: '/images/Playing_Board_Games.png',
+    soundSrc: '/sounds/Playing_Board_Games.mp3'
+  },
+  {
+    word: 'Playing Video Games',
+    imageSrc: '/images/Playing_VideoGames.png',
+    soundSrc: '/sounds/Playing_VideoGames.mp3'
+  },
+  {
+    word: 'Listening To Music',
+    imageSrc: '/images/Listening_To_Music.png',
+    soundSrc: '/sounds/Listening_To_Music.mp3'
+  },
+  {
+    word: 'Watching TV',
+    imageSrc: '/images/Watching_TV.png',
+    soundSrc: '/sounds/Watching_TV.mp3'
+  },
+  {
+    word: 'Reading',
+    imageSrc: '/images/Reading.png',
+    soundSrc: '/sounds/Reading.mp3'
+  },
+  {
+    word: 'Writing',
+    imageSrc: '/images/Writing.png',
+    soundSrc: '/sounds/Writing.mp3'
+  },
+  {
+    word: 'Studying',
+    imageSrc: '/images/Studying.png',
+    soundSrc: '/sounds/Studying.mp3'
+  },
+  {
+    word: 'Doing Yoga',
+    imageSrc: '/images/Doing_Yoga.png',
+    soundSrc: '/sounds/Doing_Yoga.mp3'
+  },
+  {
+    word: 'Dancing',
+    imageSrc: '/images/Dancing.png',
+    soundSrc: '/sounds/Dancing.mp3'
+  },
+  {
+    word: 'Singing',
+    imageSrc: '/images/Singing.png',
+    soundSrc: '/sounds/Singing.mp3'
+  },
+  {
+    word: 'Painting',
+    imageSrc: '/images/Painting.png',
+    soundSrc: '/sounds/Painting.mp3'
+  },
+  {
+    word: 'Cooking',
+    imageSrc: '/images/Cooking.png',
+    soundSrc: '/sounds/Cooking.mp3'
+  },
+  {
+    word: 'Snorkeling',
+    imageSrc: '/images/Snorkeling.png',
+    soundSrc: '/sounds/Snorkeling.mp3'
+  },
+  {
+    word: 'Playing Piano',
+    imageSrc: '/images/Playing_Piano.png',
+    soundSrc: '/sounds/Playing_Piano.mp3'
+  },
+  {
+    word: 'Playing Soccer',
+    imageSrc: '/images/Playing_Soccer.png',
+    soundSrc: '/sounds/Playing_Soccer.mp3'
+  },
+  {
+    word: 'Playing Guitar',
+    imageSrc: '/images/Playing_Guitar.png',
+    soundSrc: '/sounds/Playing_Guitar.mp3'
+  },
+  {
+    word: 'Doing Karate',
+    imageSrc: '/images/Doing_Karate.png',
+    soundSrc: '/sounds/Doing_Karate.mp3'
+  },
+  {
+    word: 'Doing Judo',
+    imageSrc: '/images/Doing_Judo.png',
+    soundSrc: '/sounds/Doing_Judo.mp3'
+  },
+  {
+    word: 'Go To The Theater',
+    imageSrc: '/images/Go_To_The_Theater.png',
+    soundSrc: '/sounds/Go_To_The_Theater.mp3'
+  },
+  {
+    word: 'Play The Fiddle',
+    imageSrc: '/images/Play_The_Fiddle.png',
+    soundSrc: '/sounds/Play_The_Fiddle.mp3'
+  },
+  {
+    word: 'Working Out',
+    imageSrc: '/images/Working_Out.png',
+    soundSrc: '/sounds/Working_Out.mp3'
+  },
+  {
+    word: 'Taking Pictures',
+    imageSrc: '/images/Taking_Pictures.png',
+    soundSrc: '/sounds/Taking_Pictures.mp3'
+  }
+  ,
+  {
+    word: 'Bird Watching',
+    imageSrc: '/images/Bird_Watching.png',
+    soundSrc: '/sounds/Bird_Watching.mp3'
+  }
 ]
