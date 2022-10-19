06ccd79d4856c2cc1e1037fcfa1e59e40fe83691
Javier Molina Herrera <javmoli045@gmail.com>
2022-10-18 21:19:16 -0600

    Modificados los archivos js de vocabulario de cuarto grado, unidad 2

--- interactive-learner/src/data/Grades/4th/Unit 2/Places and People at the Park/vocabulary.js
+++ interactive-learner/src/data/Grades/4th/Unit 2/Places and People at the Park/vocabulary.js
@@ -1,135 +1,200 @@
 export const vocabulary = [
 
-    {
-      word: 'Landmarks',
-      imageSrc: '/images/Landmarks.png',
-      soundSrc: '/sounds/Landmarks.mp3'
-    },
-    
-    
-    {
-      word: 'Wildlife',
-      imageSrc: '/images/Wildlife.png',
-      soundSrc: '/sounds/Wildlife.mp3'
-    },
-    {
-      word: 'Forest',
-      imageSrc: '/images/Forest.png',
-      soundSrc: '/sounds/Forest.mp3'
-    },
-    {
-      word: 'Warning Signs',
-      imageSrc: '/images/Warning Signs.png',
-      soundSrc: '/sounds/Warning Signs.mp3'
-    },
-    {
-      word: 'Restrooms',
-      imageSrc: '/images/Restrooms.png',
-      soundSrc: '/sounds/Restrooms.mp3'
-    },
-    {
-      word: 'Picnic areas',
-      imageSrc: '/images/Picnic areas.png',
-      soundSrc: '/sounds/Picnic areas.mp3'
-    },
-    {
-      word: 'Flora',
-      imageSrc: '/images/Flora.png',
-      soundSrc: '/sounds/Flora.mp3'
-    },
-    {
-      word: 'Fauna',
-      imageSrc: '/images/Fauna.png',
-      soundSrc: '/sounds/Fauna.mp3'
-    },
-    {
-      word: 'Mountain',
-      imageSrc: '/images/Mountain.png',
-      soundSrc: '/sounds/Mountain.mp3'
-    },
-    {
-      word: 'Waterfall',
-      imageSrc: '/images/Waterfall.png',
-      soundSrc: '/sounds/Waterfall.mp3'
-    },
-    {
-      word: 'Canyon',
-      imageSrc: '/images/Canyon.png',
-      soundSrc: '/sounds/Canyon.mp3'
-    },
-    {
-      word: 'Camping',
-      imageSrc: '/images/Camping.png',
-      soundSrc: '/sounds/Camping.mp3'
-    },
-    {
-      word: 'Hiking',
-      imageSrc: '/images/Hiking.png',
-      soundSrc: '/sounds/Hiking.mp3'
-    },
-    {
-      word: 'Plants',
-      imageSrc: '/images/Plants.png',
-      soundSrc: '/sounds/Plants.mp3'
-    },
-    {
-      word: 'Lake',
-      imageSrc: '/images/Lake.png',
-      soundSrc: '/sounds/Lake.mp3'
-    },
-    {
-      word: 'Flower',
-      imageSrc: '/images/Flower.png',
-      soundSrc: '/sounds/Flower.mp3'
-    },
-    {
-      word: 'Ecosystem',
-      imageSrc: '/images/Ecosystem.png',
-      soundSrc: '/sounds/Ecosystem.mp3'
-    },
-    {
-      word: 'River',
-      imageSrc: '/images/River.png',
-      soundSrc: '/sounds/River.mp3'
-    },
-    {
-      word: 'Clean',
-      imageSrc: '/images/Clean.png',
-      soundSrc: '/sounds/Clean.mp3'
-    },
-    {
-      word: 'Peaceful',
-      imageSrc: '/images/Peaceful.png',
-      soundSrc: '/sounds/Peaceful.mp3'
-    },
-    {
-      word: 'Blooming',
-      imageSrc: '/images/Blooming.png',
-      soundSrc: '/sounds/Blooming.mp3'
-    },
-    {
-      word: 'Majestic',
-      imageSrc: '/images/Majestic.png',
-      soundSrc: '/sounds/Majestic.mp3'
-    },
-    {
-      word: 'Danger',
-      imageSrc: '/images/Danger.png',
-      soundSrc: '/sounds/Danger.mp3'
-    },
-    {
-      word: 'Park ranger',
-      imageSrc: '/images/Park ranger.png',
-      soundSrc: '/sounds/Park ranger.mp3'
-    },
-    {
-      word: 'Tour Guide',
-      imageSrc: '/images/Tour Guide.png',
-      soundSrc: '/sounds/Tour Guide.mp3'
-    },
-    {
-      word: 'Tourist',
-      imageSrc: '/images/Tourist.png',
-      soundSrc: '/sounds/Tourist.mp3'
-    }
+  {
+    word: 'Landmarks',
+    imageSrc: '/images/Landmarks.png',
+    soundSrc: '/sounds/Landmarks.mp3'
+  },
+
+
+  {
+    word: 'Wildlife',
+    imageSrc: '/images/Wildlife.png',
+    soundSrc: '/sounds/Wildlife.mp3'
+  },
+  {
+    word: 'Forest',
+    imageSrc: '/images/Forest.png',
+    soundSrc: '/sounds/Forest.mp3'
+  },
+  {
+    word: 'Warning Signs',
+    imageSrc: '/images/Warning_Signs.png',
+    soundSrc: '/sounds/Warning_Signs.mp3'
+  },
+  {
+    word: 'Restrooms',
+    imageSrc: '/images/Restrooms.png',
+    soundSrc: '/sounds/Restrooms.mp3'
+  },
+  {
+    word: 'Picnic Areas',
+    imageSrc: '/images/Picnic_Areas.png',
+    soundSrc: '/sounds/Picnic_Areas.mp3'
+  },
+  {
+    word: 'Flora',
+    imageSrc: '/images/Flora.png',
+    soundSrc: '/sounds/Flora.mp3'
+  },
+  {
+    word: 'Fauna',
+    imageSrc: '/images/Fauna.png',
+    soundSrc: '/sounds/Fauna.mp3'
+  },
+  {
+    word: 'Mountain',
+    imageSrc: '/images/Mountain.png',
+    soundSrc: '/sounds/Mountain.mp3'
+  },
+  {
+    word: 'Waterfall',
+    imageSrc: '/images/Waterfall.png',
+    soundSrc: '/sounds/Waterfall.mp3'
+  },
+  {
+    word: 'Canyon',
+    imageSrc: '/images/Canyon.png',
+    soundSrc: '/sounds/Canyon.mp3'
+  },
+  {
+    word: 'Camping',
+    imageSrc: '/images/Camping.png',
+    soundSrc: '/sounds/Camping.mp3'
+  },
+  {
+    word: 'Hiking',
+    imageSrc: '/images/Hiking.png',
+    soundSrc: '/sounds/Hiking.mp3'
+  },
+  {
+    word: 'Plants',
+    imageSrc: '/images/Plants.png',
+    soundSrc: '/sounds/Plants.mp3'
+  },
+  {
+    word: 'Lake',
+    imageSrc: '/images/Lake.png',
+    soundSrc: '/sounds/Lake.mp3'
+  },
+  {
+    word: 'Flower',
+    imageSrc: '/images/Flower.png',
+    soundSrc: '/sounds/Flower.mp3'
+  },
+  {
+    word: 'Ecosystem',
+    imageSrc: '/images/Ecosystem.png',
+    soundSrc: '/sounds/Ecosystem.mp3'
+  },
+  {
+    word: 'River',
+    imageSrc: '/images/River.png',
+    soundSrc: '/sounds/River.mp3'
+  },
+  {
+    word: 'Clean',
+    imageSrc: '/images/Clean.png',
+    soundSrc: '/sounds/Clean.mp3'
+  },
+  {
+    word: 'Peaceful',
+    imageSrc: '/images/Peaceful.png',
+    soundSrc: '/sounds/Peaceful.mp3'
+  },
+  {
+    word: 'Blooming',
+    imageSrc: '/images/Blooming.png',
+    soundSrc: '/sounds/Blooming.mp3'
+  },
+  {
+    word: 'Majestic',
+    imageSrc: '/images/Majestic.png',
+    soundSrc: '/sounds/Majestic.mp3'
+  },
+  {
+    word: 'Danger',
+    imageSrc: '/images/Danger.png',
+    soundSrc: '/sounds/Danger.mp3'
+  },
+  {
+    word: 'Park Ranger',
+    imageSrc: '/images/Park_Ranger.png',
+    soundSrc: '/sounds/Park_Ranger.mp3'
+  },
+  {
+    word: 'Tour Guide',
+    imageSrc: '/images/Tour_Guide.png',
+    soundSrc: '/sounds/Tour_Guide.mp3'
+  },
+  {
+    word: 'Tourist',
+    imageSrc: '/images/Tourist.png',
+    soundSrc: '/sounds/Tourist.mp3'
+  },
+  {
+    word: 'Campfire',
+    imageSrc: '/images/Campfire.png',
+    soundSrc: '/sounds/Campfire.mp3'
+  },
+  {
+    word: 'Water',
+    imageSrc: '/images/Water.png',
+    soundSrc: '/sounds/Water.mp3'
+  },
+  {
+    word: 'Rock',
+    imageSrc: '/images/Rock.png',
+    soundSrc: '/sounds/Rock.mp3'
+  },
+  {
+    word: 'Sky',
+    imageSrc: '/images/Sky.png',
+    soundSrc: '/sounds/Sky.mp3'
+  },
+  {
+    word: 'Sun',
+    imageSrc: '/images/Sun.png',
+    soundSrc: '/sounds/Sun.mp3'
+  },
+  {
+    word: 'Moon',
+    imageSrc: '/images/Moon.png',
+    soundSrc: '/sounds/Moon.mp3'
+  },
+  {
+    word: 'Tree',
+    imageSrc: '/images/Tree.png',
+    soundSrc: '/sounds/Tree.mp3'
+  },
+  {
+    word: 'Bush',
+    imageSrc: '/images/Bush.png',
+    soundSrc: '/sounds/Bush.mp3'
+  },
+  {
+    word: 'Insects',
+    imageSrc: '/images/Insects.png',
+    soundSrc: '/sounds/Insects.mp3'
+  },
+  {
+    word: 'Volcano',
+    imageSrc: '/images/Volcano.png',
+    soundSrc: '/sounds/Volcano.mp3'
+  },
+  {
+    word: 'Jungle',
+    imageSrc: '/images/Jungle.png',
+    soundSrc: '/sounds/Jungle.mp3'
+  },
+  {
+    word: 'Swamp',
+    imageSrc: '/images/Swamp.png',
+    soundSrc: '/sounds/Swamp.mp3'
+  },
+  {
+    word: 'Cave',
+    imageSrc: '/images/Cave.png',
+    soundSrc: '/sounds/Cave.mp3'
+  }
 ]
--- interactive-learner/src/data/Grades/4th/Unit 2/Wildlife at the Park/vocabulary.js
+++ interactive-learner/src/data/Grades/4th/Unit 2/Wildlife at the Park/vocabulary.js
@@ -1,155 +1,223 @@
 export const vocabulary = [
 
-    {
-      word: 'Turtles',
-      imageSrc: '/images/Turtles.png',
-      soundSrc: '/sounds/Turtles.mp3'
-    },
-    
-    
-    {
-      word: 'Sloths',
-      imageSrc: '/images/Sloths.png',
-      soundSrc: '/sounds/Sloths.mp3'
-    },
-    {
-      word: 'Monkeys',
-      imageSrc: '/images/Monkeys.png',
-      soundSrc: '/sounds/Monkeys.mp3'
-    },
-    {
-      word: 'Snakes',
-      imageSrc: '/images/Snakes.png',
-      soundSrc: '/sounds/Snakes.mp3'
-    },
-    {
-      word: 'Squirrels',
-      imageSrc: '/images/Squirrels.png',
-      soundSrc: '/sounds/Squirrels.mp3'
-    },
-    {
-      word: 'Parrots',
-      imageSrc: '/images/Parrots.png',
-      soundSrc: '/sounds/Parrots.mp3'
-    },
-    {
-      word: 'Scarlet',
-      imageSrc: '/images/Scarlet.png',
-      soundSrc: '/sounds/Scarlet.mp3'
-    },
-    {
-      word: 'Macaws',
-      imageSrc: '/images/Macaws.png',
-      soundSrc: '/sounds/Macaws.mp3'
-    },
-    {
-      word: 'Crabs',
-      imageSrc: '/images/Crabs.png',
-      soundSrc: '/sounds/Crabs.mp3'
-    },
-    {
-      word: 'Crocodiles',
-      imageSrc: '/images/Crocodiles.png',
-      soundSrc: '/sounds/Crocodiles.mp3'
-    },
-    {
-      word: 'Raccoons',
-      imageSrc: '/images/Raccoons.png',
-      soundSrc: '/sounds/Raccoons.mp3'
-    },
-    {
-      word: 'Spiders',
-      imageSrc: '/images/Spiders.png',
-      soundSrc: '/sounds/Spiders.mp3'
-    },
-    {
-      word: 'Frogs',
-      imageSrc: '/images/Frogs.png',
-      soundSrc: '/sounds/Frogs.mp3'
-    },
-    {
-      word: 'Toucans',
-      imageSrc: '/images/Toucans.png',
-      soundSrc: '/sounds/Toucans.mp3'
-    },
-    {
-      word: 'Iguanas',
-      imageSrc: '/images/Iguanas.png',
-      soundSrc: '/sounds/Iguanas.mp3'
-    },
-    {
-      word: 'Hummingbirds',
-      imageSrc: '/images/Hummingbirds.png',
-      soundSrc: '/sounds/Hummingbirds.mp3'
-    },
-    {
-      word: 'Butterflies',
-      imageSrc: '/images/Butterflies.png',
-      soundSrc: '/sounds/Butterflies.mp3'
-    },
-    {
-      word: 'Beetles',
-      imageSrc: '/images/Beetles.png',
-      soundSrc: '/sounds/Beetles.mp3'
-    },
-    {
-      word: 'Tapirs',
-      imageSrc: '/images/Tapirs.png',
-      soundSrc: '/sounds/Tapirs.mp3'
-    },
-    {
-      word: 'Anteaters',
-      imageSrc: '/images/Anteaters.png',
-      soundSrc: '/sounds/Anteaters.mp3'
-    },
-    {
-      word: 'Bats',
-      imageSrc: '/images/Bats.png',
-      soundSrc: '/sounds/Bats.mp3'
-    },
-    {
-      word: 'Toads',
-      imageSrc: '/images/Toads.png',
-      soundSrc: '/sounds/Toads.mp3'
-    },
-    {
-      word: 'Coatimundis',
-      imageSrc: '/images/Coatimundis.png',
-      soundSrc: '/sounds/Coatimundis.mp3'
-    },
-    {
-      word: 'Dolphins',
-      imageSrc: '/images/Dolphins.png',
-      soundSrc: '/sounds/Dolphins.mp3'
-    },
-    {
-      word: 'Otters',
-      imageSrc: '/images/Otters.png',
-      soundSrc: '/sounds/Otters.mp3'
-    },
-    {
-      word: 'Jaguars',
-      imageSrc: '/images/Jaguars.png',
-      soundSrc: '/sounds/Jaguars.mp3'
-    },
-    {
-      word: 'Pumas',
-      imageSrc: '/images/Pumas.png',
-      soundSrc: '/sounds/Pumas.mp3'
-    },
-    {
-      word: 'Panthers',
-      imageSrc: '/images/Panthers.png',
-      soundSrc: '/sounds/Panthers.mp3'
-    },
-    {
-      word: 'Whales',
-      imageSrc: '/images/Whales.png',
-      soundSrc: '/sounds/Whales.mp3'
-    },
-    {
-      word: 'Manatees',
-      imageSrc: '/images/Manatees.png',
-      soundSrc: '/sounds/Manatees.mp3'
-    }
+  {
+    word: 'Turtle',
+    imageSrc: '/images/Turtle.png',
+    soundSrc: '/sounds/Turtle.mp3'
+  },
+  {
+    word: 'Sloth',
+    imageSrc: '/images/Sloth.png',
+    soundSrc: '/sounds/Sloth.mp3'
+  },
+  {
+    word: 'Monkey',
+    imageSrc: '/images/Monkey.png',
+    soundSrc: '/sounds/Monkey.mp3'
+  },
+  {
+    word: 'Snake',
+    imageSrc: '/images/Snake.png',
+    soundSrc: '/sounds/Snake.mp3'
+  },
+  {
+    word: 'Squirrel',
+    imageSrc: '/images/Squirrel.png',
+    soundSrc: '/sounds/Squirrel.mp3'
+  },
+  {
+    word: 'Parakeet',
+    imageSrc: '/images/Parakeet.png',
+    soundSrc: '/sounds/Parakeet.mp3'
+  },
+  {
+    word: 'Macaw',
+    imageSrc: '/images/Macaw.png',
+    soundSrc: '/sounds/Macaw.mp3'
+  },
+  {
+    word: 'Crab',
+    imageSrc: '/images/Crab.png',
+    soundSrc: '/sounds/Crab.mp3'
+  },
+  {
+    word: 'Crocodile',
+    imageSrc: '/images/Crocodile.png',
+    soundSrc: '/sounds/Crocodile.mp3'
+  },
+  {
+    word: 'Raccoon',
+    imageSrc: '/images/Raccoon.png',
+    soundSrc: '/sounds/Raccoon.mp3'
+  },
+  {
+    word: 'Spider',
+    imageSrc: '/images/Spider.png',
+    soundSrc: '/sounds/Spider.mp3'
+  },
+  {
+    word: 'Frog',
+    imageSrc: '/images/Frog.png',
+    soundSrc: '/sounds/Frog.mp3'
+  },
+  {
+    word: 'Toucan',
+    imageSrc: '/images/Toucan.png',
+    soundSrc: '/sounds/Toucan.mp3'
+  },
+  {
+    word: 'Iguana',
+    imageSrc: '/images/Iguana.png',
+    soundSrc: '/sounds/Iguana.mp3'
+  },
+  {
+    word: 'Hummingbird',
+    imageSrc: '/images/Hummingbird.png',
+    soundSrc: '/sounds/Hummingbird.mp3'
+  },
+  {
+    word: 'Butterfly',
+    imageSrc: '/images/Butterfly.png',
+    soundSrc: '/sounds/Butterfly.mp3'
+  },
+  {
+    word: 'Beetle',
+    imageSrc: '/images/Beetle.png',
+    soundSrc: '/sounds/Beetle.mp3'
+  },
+  {
+    word: 'Tapir',
+    imageSrc: '/images/Tapir.png',
+    soundSrc: '/sounds/Tapir.mp3'
+  },
+  {
+    word: 'Anteater',
+    imageSrc: '/images/Anteater.png',
+    soundSrc: '/sounds/Anteater.mp3'
+  },
+  {
+    word: 'Bat',
+    imageSrc: '/images/Bat.png',
+    soundSrc: '/sounds/Bat.mp3'
+  },
+  {
+    word: 'Dolphin',
+    imageSrc: '/images/Dolphin.png',
+    soundSrc: '/sounds/Dolphin.mp3'
+  },
+  {
+    word: 'Otter',
+    imageSrc: '/images/Otter.png',
+    soundSrc: '/sounds/Otter.mp3'
+  },
+  {
+    word: 'Jaguar',
+    imageSrc: '/images/Jaguar.png',
+    soundSrc: '/sounds/Jaguar.mp3'
+  },
+  {
+    word: 'Puma',
+    imageSrc: '/images/Puma.png',
+    soundSrc: '/sounds/Puma.mp3'
+  },
+  {
+    word: 'Panther',
+    imageSrc: '/images/Panther.png',
+    soundSrc: '/sounds/Panther.mp3'
+  },
+  {
+    word: 'Chameleon',
+    imageSrc: '/images/Chameleon.png',
+    soundSrc: '/sounds/Chameleon.mp3'
+  },
+  {
+    word: 'Manatee',
+    imageSrc: '/images/Manatee.png',
+    soundSrc: '/sounds/Manatee.mp3'
+  },
+  {
+    word: 'Bear',
+    imageSrc: '/images/Bear.png',
+    soundSrc: '/sounds/Bear.mp3'
+  },
+  {
+    word: 'Jellyfish',
+    imageSrc: '/images/Jellyfish.png',
+    soundSrc: '/sounds/Jellyfish.mp3'
+  },
+  {
+    word: 'Giraffe',
+    imageSrc: '/images/Giraffe.png',
+    soundSrc: '/sounds/Giraffe.mp3'
+  },
+  {
+    word: 'Fish',
+    imageSrc: '/images/Fish.png',
+    soundSrc: '/sounds/Fish.mp3'
+  },
+  {
+    word: 'Elephant',
+    imageSrc: '/images/Elephant.png',
+    soundSrc: '/sounds/Elephant.mp3'
+  },
+  {
+    word: 'Duck',
+    imageSrc: '/images/Duck.png',
+    soundSrc: '/sounds/Duck.mp3'
+  },
+  {
+    word: 'Worm',
+    imageSrc: '/images/Worm.png',
+    soundSrc: '/sounds/Worm.mp3'
+  },
+  {
+    word: 'Bee',
+    imageSrc: '/images/Bee.png',
+    soundSrc: '/sounds/Bee.mp3'
+  },
+  {
+    word: 'Ladybug',
+    imageSrc: '/images/Ladybug.png',
+    soundSrc: '/sounds/Ladybug.mp3'
+  },
+  {
+    word: 'Whale',
+    imageSrc: '/images/Whale.png',
+    soundSrc: '/sounds/Whale.mp3'
+  },
+  {
+    word: 'Shark',
+    imageSrc: '/images/Shark.png',
+    soundSrc: '/sounds/Shark.mp3'
+  },
+  {
+    word: 'Ant',
+    imageSrc: '/images/Ant.png',
+    soundSrc: '/sounds/Ant.mp3'
+  },
+  {
+    word: 'Firefly',
+    imageSrc: '/images/Firefly.png',
+    soundSrc: '/sounds/Firefly.mp3'
+  },
+  {
+    word: 'Moth',
+    imageSrc: '/images/Moth.png',
+    soundSrc: '/sounds/Moth.mp3'
+  },
+  {
+    word: 'Stingray',
+    imageSrc: '/images/Stingray.png',
+    soundSrc: '/sounds/Stingray.mp3'
+  },
+  {
+    word: 'Firefly',
+    imageSrc: '/images/Firefly.png',
+    soundSrc: '/sounds/Firefly.mp3'
+  },
+  {
+    word: 'Firefly',
+    imageSrc: '/images/Firefly.png',
+    soundSrc: '/sounds/Firefly.mp3'
+  }
 ]
