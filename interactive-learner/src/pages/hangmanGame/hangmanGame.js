cefc656106f848d2858ce995fed91196208d4715
Sebastian Alfaro <alfaroleonsebas97@gmail.com>
2022-11-19 17:03:35 -0600

    teclado virtual

--- interactive-learner/src/pages/hangmanGame/hangmanGame.js
+++ interactive-learner/src/pages/hangmanGame/hangmanGame.js
@@ -27,15 +27,18 @@ export const HangmanGame = () => {
   const [theme] = useState(useSelector((state) => state.theme.selectedTheme.Theme));
   const [words] = useState(Themes.find(element => element.name === theme).words);
   const [selectedWord, setSelectedWord] = useState(selectWord(words));
-  
+
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
   const [showNotification, setShowNotification] = useState(false);
+  
+  const [display, setDisplay] = useState('Display Keyboard');
 
   useEffect(() => {
     setCorrectLetters(currentLetters => [...currentLetters, ' ']);
-  },[]);
+    document.getElementById('keyboard').style.display = 'none';
+  }, []);
 
   useEffect(() => {
     console.log(selectedWord);
@@ -61,7 +64,7 @@ export const HangmanGame = () => {
     window.addEventListener('keydown', handleKeydown);
 
     return () => window.removeEventListener('keydown', handleKeydown);
-  }, [correctLetters, wrongLetters, playable, selectedWord]);
+  }, [correctLetters, wrongLetters, playable, selectedWord, display]);
 
 
   const playAgain = () => {
@@ -82,6 +85,13 @@ export const HangmanGame = () => {
     setTimeout(() => {
       setter(false);
     }, 2000);
+  }
+
+  const displayKeyboard = () => {
+    const element = document.getElementById('keyboard')
+    element.style.display = (element.style.display === 'none' ? 'block' : 'none');
+    console.log(element.style.display);
+    setDisplay( (display === 'Display Keyboard' ? 'Hide Keyboard' : 'Display Keyboard') );
   }
 
   return (
@@ -106,8 +116,47 @@ export const HangmanGame = () => {
                 <WrongLetters wrongLetters={wrongLetters} />
               </div>
               <Word selectedWord={selectedWord} correctLetters={correctLetters} />
-              <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playable={playable}/>
+              <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playable={playable} />
               <Notification showNotification={showNotification} />
+
+              <button onClick={displayKeyboard}>{display}</button>
+              
+              <div id="keyboard">
+                <div className="keyboard__row">
+                  <div className="key--letter" data-char="Q">Q</div>
+                  <div className="key--letter" data-char="W">W</div>
+                  <div className="key--letter" data-char="E">E</div>
+                  <div className="key--letter" data-char="R">R</div>
+                  <div className="key--letter" data-char="T">T</div>
+                  <div className="key--letter" data-char="Y">Y</div>
+                  <div className="key--letter" data-char="U">U</div>
+                  <div className="key--letter" data-char="I">I</div>
+                  <div className="key--letter" data-char="O">O</div>
+                  <div className="key--letter" data-char="P">P</div>
+
+                </div>
+                <div className="keyboard__row">
+                  <div className="key--letter" data-char="A">A</div>
+                  <div className="key--letter" data-char="S">S</div>
+                  <div className="key--letter" data-char="D">D</div>
+                  <div className="key--letter" data-char="F">F</div>
+                  <div className="key--letter" data-char="G">G</div>
+                  <div className="key--letter" data-char="H">H</div>
+                  <div className="key--letter" data-char="J">J</div>
+                  <div className="key--letter" data-char="K">K</div>
+                  <div className="key--letter" data-char="L">L</div>
+                </div>
+                <div className="keyboard__row">
+                  <div className="key--letter" data-char="Z">Z</div>
+                  <div className="key--letter" data-char="X">X</div>
+                  <div className="key--letter" data-char="C">C</div>
+                  <div className="key--letter" data-char="V">V</div>
+                  <div className="key--letter" data-char="B">B</div>
+                  <div className="key--letter" data-char="N">N</div>
+                  <div className="key--letter" data-char="M">M</div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
