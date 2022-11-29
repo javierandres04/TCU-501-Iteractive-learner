d707733cd6ff2eddbf53f192e14b22f7766a2327
Sebastian Alfaro <alfaroleonsebas97@gmail.com>
2022-11-28 18:11:20 -0600

    press a key

--- interactive-learner/src/components/HangmanComponents/Word.js
+++ interactive-learner/src/components/HangmanComponents/Word.js
@@ -3,22 +3,26 @@ import React from 'react';
 const Word = ({ selectedWord, correctLetters }) => {
 
   return (
-    <div className="word">
-      {selectedWord.split('').map((letter, i) => {
-        if(letter !== ' '){
-          return (
-            <span className="letter" key={i}>
-              {correctLetters.includes(letter) ? letter : ''}
-            </span>
-          )
-        }else{
-          return (
-            <span className="space" key={i}>
-              {correctLetters.includes(letter) ? letter : ''}
-            </span>
-          )
-        }
-      })}
+    <div id='pressAKeyContainer'>
+      <div id='pressAKey'>Press a Key
+        <div className='dots'></div></div>
+      <div className="word">
+        {selectedWord.split('').map((letter, i) => {
+          if (letter !== ' ') {
+            return (
+              <span className="letter" key={i}>
+                {correctLetters.includes(letter) ? letter : ''}
+              </span>
+            )
+          } else {
+            return (
+              <span className="space" key={i}>
+                {correctLetters.includes(letter) ? letter : ''}
+              </span>
+            )
+          }
+        })}
+      </div>
     </div>
   )
 }
