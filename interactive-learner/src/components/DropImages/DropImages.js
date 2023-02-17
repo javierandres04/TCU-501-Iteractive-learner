3d854dc56bf8b2d1a4176a3a756a7c55eeb6fa1b
Fabian <faalgoro@gmail.com>
2023-02-17 16:12:47 -0600

    Fixed a bug in the DragAndDrop Game where the dropped image was appearing in the wrong position

--- interactive-learner/src/components/DropImages/DropImages.js
+++ interactive-learner/src/components/DropImages/DropImages.js
@@ -7,16 +7,27 @@ const format = ".png"
 const df = "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png"
 
 var placeholder = [df,df,df,df,df,df,df,df]
+var names_order = []
 
+/**
+ * Receives the information from the image dropped in the position
+ * that matched the key.
+ * 
+ * Looks in the names_order the position where the name is equal and
+ * updates the appearence of that box.
+ */
 const changePlacerholder = (e) => {
   e.containerElem.style.visibility = 'hidden';
-  placeholder[e.dragData[1]] = route+e.dragData[0]+format;
+  names_order.forEach((word, index)=>{
+    if(e.dragData[0] == word.word) {  
+      placeholder[index] = route+e.dragData[0]+format;
+    }
+  })
 }
-
-
 
 export const DropImages = ({ words }) => {
   let wordList = [];
+  names_order = words;
   words.forEach((word, index)=>{
     wordList.push( 
       <DropTarget targetKey={word.word} onHit={changePlacerholder}>
