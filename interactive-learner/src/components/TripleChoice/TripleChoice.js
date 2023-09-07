512c58e3d542008480134a9e871fdbc9e4b0d6bc
fabian883 <faalgoro@gmail.com>
2023-09-07 11:02:02 -0600

    Fix selection of words being done each time an attemp was added

--- interactive-learner/src/components/TripleChoice/TripleChoice.js
+++ interactive-learner/src/components/TripleChoice/TripleChoice.js
@@ -6,24 +6,52 @@ const route = "../../.."
 const handleClickedImage = (index, rightChoice, addMatch) => {
   console.log("HandleClick " + index);
   if(index === rightChoice) {
-  addMatch();
-  console.log("\tRight choice selected");
+    addMatch();
+    console.log("\tRight choice selected");
   } else {
     console.log("\tWrong choice selected");
   }
 };
 
+const randomizeOptions  = (words, selectedWords, rightChoice, setRightChoice, matches) => {
+  console.log("Selected Words: ");
+  console.log(selectedWords);
+  let randomChoices = []
+  let options = [];
+  setRightChoice(Math.floor(Math.random() * 3)); // position in which the right choice is
+  options = options.slice(0, 2);
+  for (let i = 0; i < words.length; i++) {
+    options[i] = i;
+  }
+  options.sort(() => Math.random() - 0.5);
+  options = options.slice(0, 2);
+  let currentOption = 0;
+  for (let i = 0; i < 3; i++) {
+    if(rightChoice == i) {
+      randomChoices[i] = selectedWords[matches];
+    } else {
+      randomChoices[i] = words[options[currentOption]];
+      ++currentOption;
+    }
+  }
+  console.log("Right choice position "+ rightChoice +"\n\tRandom choices: ");
+  console.log(randomChoices);
+  return randomChoices;
+}
+
 /**
  * Receives an array of 3 image routes and a number for the right choice
  */
-export const TripleChoice = ({ words, rightChoice, playSelectSound, addAttemp, addMatch }) => {
+export const TripleChoice = ({ words, selectedWords, rightChoice, setRightChoice, playSelectSound, addAttemp, addMatch, matches }) => {
+  let randomChoices = randomizeOptions(words[0], selectedWords, rightChoice, setRightChoice, matches);
     return (
       <ul>
+        <div id='currentWord'>Current Word: {randomChoices[rightChoice].word}</div>
         <div id = "cards-container">
           <div id = "cards-grid">
-            <img id = "card" src={route+words[0].imageSrc} alt={words.word} onClick={() => (handleClickedImage(0, rightChoice, addMatch), playSelectSound(), addAttemp())} style={{ cursor: 'pointer' }}></img>
-            <img id = "card" src={route+words[1].imageSrc} alt={words.word} onClick={() => (handleClickedImage(1, rightChoice, addMatch), playSelectSound(), addAttemp())} style={{ cursor: 'pointer' }}></img>
-            <img id = "card" src={route+words[2].imageSrc} alt={words.word} onClick={() => (handleClickedImage(2, rightChoice, addMatch), playSelectSound(), addAttemp())} style={{ cursor: 'pointer' }}></img>
+            <img id = "card" src={route+randomChoices[0].imageSrc} alt={randomChoices[0].word} onClick={() => (handleClickedImage(0, rightChoice, addMatch), playSelectSound(), addAttemp())} style={{ cursor: 'pointer' }}></img>
+            <img id = "card" src={route+randomChoices[1].imageSrc} alt={randomChoices[1].word} onClick={() => (handleClickedImage(1, rightChoice, addMatch), playSelectSound(), addAttemp())} style={{ cursor: 'pointer' }}></img>
+            <img id = "card" src={route+randomChoices[2].imageSrc} alt={randomChoices[2].word} onClick={() => (handleClickedImage(2, rightChoice, addMatch), playSelectSound(), addAttemp())} style={{ cursor: 'pointer' }}></img>
           </div>
         </div>
       </ul>
--- interactive-learner/src/pages/chooseBetweenGame/chooseBetweenGame.js
+++ interactive-learner/src/pages/chooseBetweenGame/chooseBetweenGame.js
@@ -2,7 +2,7 @@ import Swal from 'sweetalert2';
 import React from 'react';
 import { Header } from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Button } from 'react';
 import { Themes } from '../../data/themes';
 import { motion } from 'framer-motion';
 import { useSelector } from 'react-redux';
@@ -12,6 +12,7 @@ import { ConfettiRain } from '../../components/ConfettiRain/ConfettiRain';
 import './chooseBetweenGame.css';
 import '../../App.css';
 import { TripleChoice } from '../../components/TripleChoice/TripleChoice';
+
 const spanishInstructions = [
   'TODO'
 ]
@@ -48,31 +49,48 @@ const playMatchSound = () => {
 }
 
 const selectWords = (words) => {
-  let options = [];
   let selectedWords = [];
-  for (let i = 0; i < words.length; i++) {
+  let options = [];
+  for (let i = 0; i < words[0].length; i++) {
     options[i] = i;
   }
   options.sort(() => Math.random() - 0.5);
   options = options.slice(0, 8);
-
   for (let i = 0; i < 8; i++) {
-    selectedWords[i] = words[options[i]];
+    selectedWords[i] = words[0][options[i]];
   }
-  console.log("selectedWords");
+  console.log("SET OF 8 WORDS SELECTED");
   console.log(selectedWords);
   return selectedWords;
+}
+
+const startWordSelection = (words, setSelectedWords, setShowChoices) => {
+  let selectedWords = [];
+  let options = [];
+  for (let i = 0; i < words[0].length; i++) {
+    options[i] = i;
+  }
+  options.sort(() => Math.random() - 0.5);
+  options = options.slice(0, 8);
+  for (let i = 0; i < 8; i++) {
+    selectedWords[i] = words[0][options[i]];
+  }
+  console.log("SET OF 8 WORDS SELECTED");
+  console.log(selectedWords);
+  setSelectedWords(selectedWords);
+  setShowChoices(true);
 }
 
 export const ChooseBetweenGame = () => {
   const theme = useState(useSelector((state) => state.theme.selectedTheme.Theme));
   const words = useState(Themes.find(element => element.name === theme[0]).words);
-  const selectedWords = useState(selectWords(words[0]));
   const [turns, setTurns] = useState(0);
   const [matches, setMatches] = useState(0);
   const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
   const [gameWin, setGameWin] = useState(false);
-  const [currentWord, setCurrentWord] = useState("error");
+  const [rightChoice, setRightChoice] = useState(0);
+  const [selectedWords, setSelectedWords] = useState([]);
+  const [showChoices, setShowChoices] = useState(false);
   
   useEffect(() => {
     if (matches > 0) {
@@ -103,7 +121,21 @@ export const ChooseBetweenGame = () => {
     setMatches(matches+1);
     console.log("Number of matches: "+matches)
   }
-
+/*
+  useEffect(() => {
+    let options = [];
+    for (let i = 0; i < words[0].length; i++) {
+      options[i] = i;
+    }
+    options.sort(() => Math.random() - 0.5);
+    options = options.slice(0, 8);
+    for (let i = 0; i < 8; i++) {
+      selectedWords[i] = words[0][options[i]];
+    }
+    console.log("SET OF 8 WORDS SELECTED");
+    console.log(selectedWords);
+  }, []);
+*/
   return (
     <motion.div
       id="mainContainer"
@@ -123,14 +155,18 @@ export const ChooseBetweenGame = () => {
         <div id="mainBox" >
           <div id='chooseBetweenContainer'>
             <HeadGames setIsHelpModalOpen={setIsHelpModalOpen} />
-            <div id='currentWord'>Current Word: {currentWord}</div>
             <div id='tripleChoice'>
-              <TripleChoice words={selectedWords[0]} rightChoice={2} playSelectSound = {playSelectSound} addAttemp={addAttemp} addMatch = {addMatch}>
-              </TripleChoice>
+              <button onClick = {() => startWordSelection(words, setSelectedWords, setShowChoices)}> Default </button>
+              { showChoices ?
+                <TripleChoice words = {words} selectedWords={selectedWords} rightChoice={rightChoice} setRightChoice = {setRightChoice} playSelectSound = {playSelectSound} addAttemp={addAttemp} addMatch = {addMatch} matches = {matches}>
+                </TripleChoice>
+                :
+                <div>text</div>
+              }
             </div>
             <div id='stats'>
-              <div id='attempts'>Matches: {matches}</div>
               <div id='attempts'>Attempts: {turns}</div>
+              <div id='attempts'>Matches: {matches}</div>
             </div>
             <button onClick={refresh}>New Game</button>
           </div>
